7fe449d0fd15aced2b15c9c570c718a6d7fe9f10
Kyle Loveless <kjloveless321@gmail.com>
2024-11-07 15:00:02 -0600

    log charCodes < 32, write to stdout, handle \r (#13)

--- main.ts
+++ main.ts
@@ -13,16 +13,25 @@ if (import.meta.main) {
   }
 
   const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
   for await (const chunk of Deno.stdin.readable) {
       const text = decoder.decode(chunk);
       const charCode = text.charCodeAt(0);
-      if (charCode == 3 || charCode == 4) {
+      if (charCode === 3 || charCode === 4) {
           disableRawMode();
           console.log("bye!");
           Deno.exit();
       }
 
-      console.log(charCode);
+      if (charCode < 32) {
+          console.log(`char code: ${charCode}`);
+      }
+
+      if (charCode === 13) {
+          await Deno.stdout.write(encoder.encode("\n"));
+      }
+
+      const bytesWritten = await Deno.stdout.write(chunk);
 
   }
 
