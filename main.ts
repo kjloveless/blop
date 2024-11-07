2084f2618de95e36f0367e48fb82d1f1e7436284
Kyle Loveless <kjloveless321@gmail.com>
2024-11-07 14:23:52 -0600

    disableRawMode on exit, be polite say bye!

--- main.ts
+++ main.ts
@@ -1,14 +1,26 @@
+export function disableRawMode() {
+    Deno.stdin.setRaw(false);
+}
+
+export function enableRawMode() {
+    Deno.stdin.setRaw(true);
+}
+
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   if (Deno.stdin.isTerminal()) {
-      Deno.stdin.setRaw(true);
+      enableRawMode();
   }
 
   const decoder = new TextDecoder();
   for await (const chunk of Deno.stdin.readable) {
       const text = decoder.decode(chunk);
       const charCode = text.charCodeAt(0);
-      if (charCode == 3 || charCode == 4) Deno.exit();
+      if (charCode == 3 || charCode == 4) {
+          disableRawMode();
+          console.log("bye!");
+          Deno.exit();
+      }
 
       console.log(charCode);
 
