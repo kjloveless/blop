3b8696cc88b3e5028018735ffe3970d991d17104
Kyle Loveless <kjloveless321@gmail.com>
2024-11-08 16:23:36 -0600

    deno fmt and emit multi-byte sequences

--- main.ts
+++ main.ts
@@ -1,45 +1,56 @@
 export function disableRawMode() {
-    Deno.stdin.setRaw(false);
+  Deno.stdin.setRaw(false);
 }
 
 export function enableRawMode() {
-    Deno.stdin.setRaw(true);
+  Deno.stdin.setRaw(true);
 }
 
 function exit(msg: string, code: number = 0) {
-    console.log(msg);
-    Deno.exit(code);
+  console.log(msg);
+  Deno.exit(code);
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   if (Deno.stdin.isTerminal()) {
-      enableRawMode();
+    enableRawMode();
   } else {
-      exit(`this isn't a tty, sorry!`)
+    exit(`this isn't a tty, sorry!`);
   }
 
   const decoder = new TextDecoder();
   const encoder = new TextEncoder();
   var bytesWritten = 0;
   for await (const chunk of Deno.stdin.readable) {
-      const text = decoder.decode(chunk);
-      const charCode = text.charCodeAt(0);
-      if (charCode === 3 || charCode === 4) {
-          disableRawMode();
-          console.log(`\nyou wrote ${bytesWritten} bytes this session!`)
-          exit(`bye`);
+    const text = decoder.decode(chunk);
+    const charCode = text.charCodeAt(0);
+
+    if (charCode === 27) {
+      const seq = new Uint8Array(3);
+      bytesWritten += await Deno.stdin.read(seq);
+      let i = 0;
+      while (i < seq.length) {
+        console.log(seq[i]);
+        i += 1;
       }
+      continue;
+    }
 
-      if (charCode < 32 || charCode > 126) {
-          console.log(`\nchar code: ${charCode}`);
-      }
+    if (charCode === 3 || charCode === 4) {
+      disableRawMode();
+      console.log(`\nyou wrote ${bytesWritten} bytes this session!`);
+      exit(`bye`);
+    }
 
-      if (charCode === 13) {
-          await Deno.stdout.write(encoder.encode("\n"));
-      }
+    if (charCode < 32 || charCode > 126) {
+      console.log(`\nchar code: ${charCode}`);
+    }
 
-      bytesWritten += await Deno.stdout.write(chunk);
+    if (charCode === 13) {
+      await Deno.stdout.write(encoder.encode("\n"));
+    }
 
+    bytesWritten += await Deno.stdout.write(chunk);
   }
 }
